2c3b12840ac9721fd0ad0e1ed2d5417c701f4a9a
bougzy <yubeespecial@gmail.com>
2026-01-21 09:33:36 +0100

    i will

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,5 +1,10 @@
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// ESM equivalent of __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Load environment variables
 dotenv.config({ path: path.join(__dirname, '../../.env') });
